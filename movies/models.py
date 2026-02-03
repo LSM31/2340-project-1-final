72204717665564ed227a680929078e065cd97610
Landon Slater <landonslater@Landons-Laptop-2.local>
2026-02-02 23:45:11 -0500

    first commit

---
+++ movies/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+from django.db import models
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie_images/')
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
+    
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reports = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.name + ' - ' + self.user.username
+    
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.review.movie.name + ' - ' + self.user.username
